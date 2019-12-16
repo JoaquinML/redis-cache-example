6f060af61d396bc90b5da3e202a5d26db05c8e66
JoaquinML <joaquinmartinez2509@gmail.com>
2019-12-16 11:58:13 -0600

    first commit

---
+++ src/test/java/com/redisexample/redisdemo/RedisdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redisexample.redisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
